9c770615c20241de687e342e2b852488c250e2bc
parseltongist <>
2021-11-07 14:49:06 +0100

    stage 3/6 removed redundant class

--- processor.py
+++ processor.py
@@ -3,32 +3,23 @@ import numpy as np
 import re
 
 
-class MatrixProcessor():
-
-    def __init__(self, matrix):
-        self.matrix = matrix
-
-    @classmethod
-    def matrix_builder(cls, dim_message=None, matrix_message=None):
-        dim = input(dim_message).strip()
-        if not re.match(r"\A\d+\s*\d+$", dim):
-            print("Invalid input!")
-            return main()
-        else:
-            n_rows, n_cols = dim.split()
-            n_rows = int(n_rows)
-            n_cols = int(n_cols)
-
-            print(matrix_message)
-            matrix = [[float(el) for el in input().split()]for _ in range(n_rows)]
-            for row in range(len(matrix)):
-                try:
-                    message = f"Invalid row length, row {row + 1}"
-                    assert len(matrix[row]) == n_cols, message
-                except AssertionError as err:
-                    print(err)
-                    return main()
-            return cls(np.array(matrix))
+def matrix_builder(dim_message=None, matrix_message=None):
+    dim = input(dim_message).strip()
+    if not re.match(r"\A\d+\s*\d+$", dim):
+        print("Invalid input!")
+        return main()
+    else:
+        n_rows, n_cols = [int(x) for x in dim.split()]
+        print(matrix_message)
+        matrix = np.array([[float(el) for el in input().split()]for _ in range(n_rows)])
+        for row in range(len(matrix)):
+            try:
+                message = f"Invalid row length, row {row + 1}"
+                assert len(matrix[row]) == matrix.shape[1], message
+            except AssertionError as err:
+                print(err)
+                return main()
+        return matrix
 
 
 def matrix_printer(result):
@@ -38,14 +29,14 @@ def matrix_printer(result):
 
 
 def add_matrices(m1, m2):
-    if not all([m1.matrix.shape[0] == m2.matrix.shape[0], m1.matrix.shape[1] == m2.matrix.shape[1]]):
+    if not all([m1.shape[0] == m2.shape[0], m1.shape[1] == m2.shape[1]]):
         print("ERROR")
         return main()
-    return np.add(m1.matrix, m2.matrix)
+    return np.add(m1, m2)
 
 
-def multiply_by_constant(m, const):
-    return m.matrix * const
+def multiply_by_constant(matrix, const):
+    return matrix * const
 
 
 def take_constant():
@@ -58,11 +49,11 @@ def take_constant():
 
 
 def multiply_matrices(m1, m2):
-    if m1.matrix.shape[1] != m2.matrix.shape[0]:
+    if m1.shape[1] != m2.shape[0]:
         print("ERROR")
         return main()
-    # return np.multiply(m1.matrix, m2.matrix)
-    return np.matmul(m1.matrix, m2.matrix)  # https://numpy.org/doc/stable/reference/generated/numpy.matmul.html
+    # return np.multiply(m1, m2)
+    return np.matmul(m1, m2)  # https://numpy.org/doc/stable/reference/generated/numpy.matmul.html
 
 
 def main():
@@ -73,18 +64,17 @@ def main():
         except ValueError:
             print("PICK THE OPTION FROM THE LIST!")
             return main()
-
         if option == 0:
             exit()
         elif option == 1:
             """ Add two matrices """
-            matrix_a = MatrixProcessor.matrix_builder("Enter size of first matrix: ", "Enter first matrix:")
-            matrix_b = MatrixProcessor.matrix_builder("Enter size of second matrix: ", "Enter second matrix:")
+            matrix_a = matrix_builder("Enter size of first matrix: ", "Enter first matrix:")
+            matrix_b = matrix_builder("Enter size of second matrix: ", "Enter second matrix:")
             result = add_matrices(matrix_a, matrix_b)
             matrix_printer(result)
         elif option == 2:
             """ Multiply matrix by a constant """
-            matrix_a = MatrixProcessor.matrix_builder("Enter size of matrix: ", "Enter matrix:")
+            matrix_a = matrix_builder("Enter size of matrix: ", "Enter matrix:")
             constant = take_constant()
             result = multiply_by_constant(matrix_a, constant)
             matrix_printer(result)
@@ -92,8 +82,8 @@ def main():
         elif option == 3:
             " Multiply two matrices "
             print("not yet implemented")
-            matrix_a = MatrixProcessor.matrix_builder("Enter size of first matrix: ", "Enter first matrix:")
-            matrix_b = MatrixProcessor.matrix_builder("Enter size of second matrix: ", "Enter second matrix:")
+            matrix_a = matrix_builder("Enter size of first matrix: ", "Enter first matrix:")
+            matrix_b = matrix_builder("Enter size of second matrix: ", "Enter second matrix:")
             result = multiply_matrices(matrix_a, matrix_b)
             matrix_printer(result)
 
